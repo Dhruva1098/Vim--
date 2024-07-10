a5207d8786674f2132345c7d2e63072694ddf4fd
Dhruva Sharma <dhruva1098@gmail.com>
2024-07-10 14:49:02 +0530

    turn off echoing and raw mode at exit

--- vim--.c
+++ vim--.c
@@ -1,3 +1,25 @@
-int main(void){
-	return 1;
+#include <unistd.h>
+#include <termios.h>
+#include <stdlib.h>
+
+struct termios orig_termios;
+
+void disable_raw_mode(void) {
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
+void enable_raw_mode(void) {
+    tcgetattr(STDIN_FILENO, &orig_termios);
+    atexit(disable_raw_mode);
+    
+    struct termios raw = orig_termios;
+    tcgetattr(STDIN_FILENO, &raw);
+    raw.c_lflag &= ~(ECHO);
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
+int main(void) {
+    enable_raw_mode();
+    
+    char c;
+    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
+    return 0;
 }
